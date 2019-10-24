ea38acd7ef286361d7339729e5a83f963f324ed9
duanmenghuan <1482996478@qq.com>
2019-10-24 16:55:11 +0800

    提交测试类

---
+++ src/test/java/com/dmh/service/impl/BaseTest.java
@@ -0,0 +1,10 @@
+package com.dmh.service.impl;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration("classpath:spring-beans.xml")
+public class BaseTest {
+}
